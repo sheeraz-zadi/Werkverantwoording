b95621d207fb7c1434afc4992e251a245e2622a8
Sheeraz Zadi <nizaris_alamut@hotmail.com>
2015-06-14 18:39:23 +0200

    Removed "Dag" inside of the menu and improved application flow.

--- Werkverantwoording/Controllers/HomeController.cs
+++ Werkverantwoording/Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Werkverantwoording.DAL;
+using Werkverantwoording.Models;
 
 namespace Werkverantwoording.Controllers
 {
     public class HomeController : Controller
     {
+        private TaskContext db = new TaskContext();
         public ActionResult Index()
-        {
-            return View();
+        {   
+            User currentUser;
+            using (var ctx = new TaskContext())
+            {
+                currentUser = ctx.Users.Where(s => s.Email == User.Identity.Name).FirstOrDefault<User>();
+            }
+
+            if (currentUser != null) { 
+            
+            if (currentUser.Role.ToString() == "Teacher")
+            {
+                return RedirectToAction("Index", "User");
+            }
+            if (currentUser.Role.ToString() == "Student")
+            {
+                return RedirectToAction("Create", "Day");
+            }
+            }
+
+            return RedirectToAction("Login", "User");
         }
 
         public ActionResult About()
