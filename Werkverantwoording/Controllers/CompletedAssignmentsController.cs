debbbeb2c0e6d23e1e8036b4fd230383685bb401
lucienv010 <lucien_bed@hotmail.com>
2015-06-16 10:06:40 +0200

    Added Completed Assignments view and controller

---
+++ Werkverantwoording/Controllers/CompletedAssignmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Werkverantwoording.Controllers
+{
+    public class CompletedAssignmentsController : Controller
+    {
+        //
+        // GET: /CompletedAssignments/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	}
+}
